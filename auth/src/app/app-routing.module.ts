8112458d45defe8475686a79f80e182ce3b1e5e9
andreas ubertini <ubertiniandreas@gmail.com>
2023-08-02 15:29:03 +0200

    Initial commit

---
+++ auth/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './pages/auth/auth.guard';
+
+const routes: Routes = [
+  {
+    path:"",
+    component: HomeComponent,
+    pathMatch:'full'
+  },
+
+  { path: 'auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [AuthGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
